a98e648f77734059e4485e3f079737d12619ec93
chrisokoh8888-code <chrisokoh8888@gmail.com>
2026-04-03 09:57:52 +0100

    Create firebase.js

---
+++ src/services/firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
