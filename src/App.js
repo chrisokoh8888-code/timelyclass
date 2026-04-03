273e162e178c5d618b9d40e98e553ea75329eea3
chrisokoh8888-code <chrisokoh8888@gmail.com>
2026-04-03 10:57:37 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,5 @@
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
-import { useEffect, useState } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 export default function App() {
-  const [user, setUser] = useState(null);
-  const auth = getAuth();
-
-  useEffect(() => {
-    onAuthStateChanged(auth, setUser);
-  }, []);
-
-  return user ? <Dashboard /> : <Login />;
+  return <Dashboard />;
 }
