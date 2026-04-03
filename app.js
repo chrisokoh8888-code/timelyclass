7f4602d59ba0b1d98b7e56fb7b1cd67a151d8357
chrisokoh8888-code <chrisokoh8888@gmail.com>
2026-04-04 00:35:51 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-alert("JS is working!");
+document.addEventListener('DOMContentLoaded', () => {
+
+  const loginBtn = document.getElementById('login-btn');
+
+  loginBtn.addEventListener('click', () => {
+    alert('Login button clicked');
+  });
+
+});
