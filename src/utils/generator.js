063a95bfb122b835b5cf07272bb1efabb7a98751
chrisokoh8888-code <chrisokoh8888@gmail.com>
2026-04-03 11:23:40 +0100

    Update generator.js

--- src/utils/generator.js
+++ src/utils/generator.js
@@ -1,21 +1,15 @@
 export function generateTimetable(form) {
-  const days = Array.from({ length: form.daysPerWeek }, (_, i) => `Day ${i + 1}`);
-  const subjects = form.subjects.split(",");
+  const days = Array.from({ length: Number(form.daysPerWeek || 5) }, (_, i) => `Day ${i + 1}`);
+  const subjects = (form.subjects || "").split(",");
 
   let table = {};
 
   days.forEach(day => {
     table[day] = [];
 
-    for (let p = 1; p <= form.periodsPerDay; p++) {
-      if (p == form.shortBreak) {
-        table[day].push("Short Break");
-      } else if (p == form.longBreak) {
-        table[day].push("Long Break");
-      } else {
-        const subject = subjects[Math.floor(Math.random() * subjects.length)];
-        table[day].push(subject);
-      }
+    for (let p = 1; p <= Number(form.periodsPerDay || 6); p++) {
+      const subject = subjects[Math.floor(Math.random() * subjects.length)] || "Free";
+      table[day].push(subject);
     }
   });
 
